40ebf90731f3b4d6a6132c85f49448fa38120b78
Syafiq Mastor <syafiqmastor@openmindsresources.com>
2017-07-29 13:10:27 +0800

    Initial Commit

---
+++ Adcov8Tests/Adcov8Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Adcov8Tests.swift
+//  Adcov8Tests
+//
+//  Created by Syafiq Mastor on 7/29/17.
+//  Copyright © 2017 syafiqmastor. All rights reserved.
+//
+
+import XCTest
+@testable import Adcov8
+
+class Adcov8Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
